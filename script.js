59b4c3e4f00f5e6c2f8c120174cc0142fcd1f7b0
tawandakasunzuma <tskasunzuma@gmail.com>
2025-05-01 17:48:22 +0200

    create prompts to add tasks in javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+// Task 1 Prompts
+
+const taskOneTitle = prompt("Enter task 1 title:");
+const taskOneDescription = prompt("Enter task 1 description:");
+const taskOneStatus = prompt("Enter task 1 status:");
+
+// Task 2 Prompts
+
+const taskTwoTitle = prompt("Enter task 2 title:");
+const taskTwoDescription = prompt("Enter task 2 description:");
+const taskTwoStatus = prompt("Enter task 2 status:");
